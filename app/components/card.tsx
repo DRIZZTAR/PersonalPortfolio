40a2011e9fb5ee29f1613d2972a11c6a5acc60d5
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-04-09 13:06:54 -0600

    gradients semi final for cards

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -23,12 +23,12 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
 			onMouseMove={onMouseMove}
-			className="overflow-hidden relative duration-700 rounded-xl hover:bg-white/30 group md:gap-8 border-zinc-300 "
+			className="overflow-hidden relative duration-1000 rounded-xl hover:bg-white/30 group md:gap-8 border-zinc-300 "
 		>
 			<div className="pointer-events-none">
 				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
 				<motion.div
-					className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-neutral-100  transition duration-1000 group-hover:opacity-90 "
+					className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-neutral-300  transition duration-1000 group-hover:opacity-100 "
 					style={style}
 				/>
 				<motion.div
