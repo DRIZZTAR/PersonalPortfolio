cb030bbe4dc21378e15a95d7789adc2cb4aaac31
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-04-09 11:15:20 -0600

    Back to black

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,6 @@ export default function Home() {
           Developer
         </h1>
       </div>
-
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div
         style={{ fontFamily: "Apple Garamond Light, sans-serif" }}
