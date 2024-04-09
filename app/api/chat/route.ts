8ae9707ed91da52c7a3fb14c1e7eeade5c2580c6
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-04-08 23:38:31 -0600

    commenting out functions for test deploy

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -112,28 +112,28 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 // Function definitions:
-const functions: ChatCompletionCreateParams.Function[] = [
-  {
-    name: "get_current_weather",
-    description: "Get the current weather",
-    parameters: {
-      type: "object",
-      properties: {
-        location: {
-          type: "string",
-          description: "The city and state, e.g. San Francisco, CA",
-        },
-        format: {
-          type: "string",
-          enum: ["celsius", "fahrenheit"],
-          description:
-            "The temperature unit to use. Infer this from the users location.",
-        },
-      },
-      required: ["location", "format"],
-    },
-  },
-];
+// const functions: ChatCompletionCreateParams.Function[] = [
+//   {
+//     name: "get_current_weather",
+//     description: "Get the current weather",
+//     parameters: {
+//       type: "object",
+//       properties: {
+//         location: {
+//           type: "string",
+//           description: "The city and state, e.g. San Francisco, CA",
+//         },
+//         format: {
+//           type: "string",
+//           enum: ["celsius", "fahrenheit"],
+//           description:
+//             "The temperature unit to use. Infer this from the users location.",
+//         },
+//       },
+//       required: ["location", "format"],
+//     },
+//   },
+// ];
 
 // And use it like this:
 export async function POST(req: Request) {
@@ -219,7 +219,7 @@ const contextMessage = {
     model: "gpt-3.5-turbo-0613",
     stream: true,
     messages: [contextMessage, ...messages],
-    functions,
+    // functions,
   });
 
   // Convert the response into a friendly text-stream
