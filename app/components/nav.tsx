d444a811bc4a712e599082f02fc0ed710ee192f1
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-04-12 20:37:58 -0600

    Fix to nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -23,8 +23,8 @@ export const Navigation: React.FC = () => {
         style={{ fontFamily: "Apple Garamond, sans-serif" }}
         className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
           isIntersecting
-            ? "bg-zinc-900/0 border-transparent"
-            : "bg-zinc-900/500  border-zinc-800 "
+            ? "bg-zinc-900/10 border-transparent"
+            : "bg-zinc-900/50  border-zinc-800 "
         }`}
       >
         <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
@@ -47,7 +47,7 @@ export const Navigation: React.FC = () => {
             href="/"
             className="duration-200 text-zinc-300 hover:text-zinc-100"
           >
-            <ArrowLeft className="w-6 h-6 " />
+            <ArrowLeft className="w-6 h-6" />
           </Link>
         </div>
       </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,7 +29,7 @@ const socials = [
 export default function ContactPage() {
   return (
     <div className=" bg-gradient-to-br from-black via-slate-400/20 to-black">
-      <Particles className="absolute inset-0 -z-10" quantity={100} />
+      <Particles className="absolute inset-0 -z-10" quantity={200} />
       <Navigation />
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
