8ab32c59b6677e52b8138adf38977f7549544aed
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-07-30 22:44:07 -0600

    added new book project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,18 +7,16 @@ import XCard from '../components/xcard';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const tweetUrls = [
+	'https://twitter.com/TysonJeremy/status/1818487260771500039',
 	'https://twitter.com/TysonJeremy/status/1799305094477054046',
 	'https://twitter.com/TysonJeremy/status/1810386385653936215',
 	'https://twitter.com/TysonJeremy/status/1809078125692924132',
 	'https://twitter.com/TysonJeremy/status/1795669324423995418',
 	'https://twitter.com/TysonJeremy/status/1808354043368202359',
 	'https://twitter.com/TysonJeremy/status/1810892631662284940',
-	'https://twitter.com/TysonJeremy/status/1806886567732244806',
 	'https://twitter.com/TysonJeremy/status/1803484767024980409',
 	'https://twitter.com/TysonJeremy/status/1797121650724688047',
 	'https://twitter.com/TysonJeremy/status/1804704053215252943',
-	'https://twitter.com/TysonJeremy/status/1802779644359524485',
-	'https://twitter.com/TysonJeremy/status/1801709673592328331',
 ];
 
 export default function ProjectsPage() {
@@ -28,7 +26,7 @@ export default function ProjectsPage() {
   useEffect(() => {
     const fetchTweets = async () => {
       try {
-        await new Promise(resolve => setTimeout(resolve, 1500));
+        await new Promise(resolve => setTimeout(resolve, 1000));
         setTweets(tweetUrls);
       } catch (error) {
         console.error('Error fetching tweets:', error);
